517a70d0a3beac39bb1c3207629b97a6ec1bad97
hooloovooblu <nick.lacock@gmail.com>
2020-11-30 18:11:51 -0800

    Create imgtojson.py

---
+++ imgtojson.py
@@ -0,0 +1,152 @@
+# Map from image names (in download_imgs.sh) to "blueprint paths" (magic strings the game uses to identify resources) 
+image_to_blueprint_path = {
+"A.I._Limiter.png":"/Game/FactoryGame/Resource/Parts/CircuitBoardHighSpeed/Desc_CircuitBoardHighSpeed.Desc_CircuitBoardHighSpeed_C",
+"Adaptive_Control_Unit.png":"/Game/FactoryGame/Resource/Parts/SpaceElevatorParts/Desc_SpaceElevatorPart_5.Desc_SpaceElevatorPart_5_C",
+"Alclad_Aluminum_Sheet.png":"/Game/FactoryGame/Resource/Parts/AluminumPlate/Desc_AluminumPlate.Desc_AluminumPlate_C",
+"Alien_Carapace.png":"/Game/FactoryGame/Resource/Parts/AnimalParts/Desc_HogParts.Desc_HogParts_C",
+"Alien_Organs.png":"/Game/FactoryGame/Resource/Parts/AnimalParts/Desc_SpitterParts.Desc_SpitterParts_C",
+"Aluminum_Ingot.png":"/Game/FactoryGame/Resource/Parts/AluminumIngot/Desc_AluminumIngot.Desc_AluminumIngot_C",
+"Aluminum_Scrap.png":"/Game/FactoryGame/Resource/Parts/AluminumScrap/Desc_AluminumScrap.Desc_AluminumScrap_C",
+"Automated_Wiring.png":"/Game/FactoryGame/Resource/Parts/SpaceElevatorParts/Desc_SpaceElevatorPart_3.Desc_SpaceElevatorPart_3_C",
+"Bacon_Agaric.png":"/Game/FactoryGame/Resource/Environment/DesertShroom/Desc_Shroom.Desc_Shroom_C",
+"Battery.png":"/Game/FactoryGame/Resource/Parts/Battery/Desc_Battery.Desc_Battery_C",
+"Bauxite.png":"/Game/FactoryGame/Resource/RawResources/OreBauxite/Desc_OreBauxite.Desc_OreBauxite_C",
+"Beacon.png":"/Game/FactoryGame/Resource/Equipment/Beacon/BP_EquipmentDescriptorBeacon.BP_EquipmentDescriptorBeacon_C",
+"Beryl_Nut.png":"/Game/FactoryGame/Resource/Environment/Nut/Desc_Nut.Desc_Nut_C",
+"Biomass.png":"/Game/FactoryGame/Resource/Parts/GenericBiomass/Desc_GenericBiomass.Desc_GenericBiomass_C",
+"Black_Powder.png":"/Game/FactoryGame/Resource/Parts/GunPowder/Desc_Gunpowder.Desc_Gunpowder_C",
+"Blade_Runners.png":"/Game/FactoryGame/Resource/Equipment/JumpingStilts/BP_EquipmentDescriptorJumpingStilts.BP_EquipmentDescriptorJumpingStilts_C",
+"Cable.png":"/Game/FactoryGame/Resource/Parts/Cable/Desc_Cable.Desc_Cable_C",
+"Caterium_Ingot.png":"/Game/FactoryGame/Resource/Parts/GoldIngot/Desc_GoldIngot.Desc_GoldIngot_C",
+"Caterium_Ore.png":"/Game/FactoryGame/Resource/RawResources/OreGold/Desc_OreGold.Desc_OreGold_C",
+"Chainsaw.png":"/Game/FactoryGame/Equipment/Chainsaw/Desc_Chainsaw.Desc_Chainsaw_C",
+"Circuit_Board.png":"/Game/FactoryGame/Resource/Parts/CircuitBoard/Desc_CircuitBoard.Desc_CircuitBoard_C",
+"Coal.png":"/Game/FactoryGame/Resource/RawResources/Coal/Desc_Coal.Desc_Coal_C",
+"Color_Cartridge.png":"/Game/FactoryGame/Resource/Parts/ColorCartridge/Desc_ColorCartridge.Desc_ColorCartridge_C",
+"Color_Gun.png":"/Game/FactoryGame/Resource/Equipment/ColorGun/BP_EquipmentDescriptorColorGun.BP_EquipmentDescriptorColorGun_C",
+"Computer.png":"/Game/FactoryGame/Resource/Parts/Computer/Desc_Computer.Desc_Computer_C",
+"Concrete.png":"/Game/FactoryGame/Resource/Parts/Cement/Desc_Cement.Desc_Cement_C",
+"Copper_Ingot.png":"/Game/FactoryGame/Resource/Parts/CopperIngot/Desc_CopperIngot.Desc_CopperIngot_C",
+"Copper_Ore.png":"/Game/FactoryGame/Resource/RawResources/OreCopper/Desc_OreCopper.Desc_OreCopper_C",
+"Copper_Sheet.png":"/Game/FactoryGame/Resource/Parts/CopperSheet/Desc_CopperSheet.Desc_CopperSheet_C",
+"Crystal_Oscillator.png":"/Game/FactoryGame/Resource/Parts/CrystalOscillator/Desc_CrystalOscillator.Desc_CrystalOscillator_C",
+"Electromagnetic_Control_Rod.png":"/Game/FactoryGame/Resource/Parts/ElectromagneticControlRod/Desc_ElectromagneticControlRod.Desc_ElectromagneticControlRod_C",
+"Empty_Canister.png":"/Game/FactoryGame/Resource/Parts/FluidCanister/Desc_FluidCanister.Desc_FluidCanister_C",
+"Encased_Industrial_Beam.png":"/Game/FactoryGame/Resource/Parts/SteelPlateReinforced/Desc_SteelPlateReinforced.Desc_SteelPlateReinforced_C",
+"FICSIT_Coupon.png":"/Game/FactoryGame/Resource/Parts/ResourceSinkCoupon/Desc_ResourceSinkCoupon.Desc_ResourceSinkCoupon_C",
+"Fabric.png":"/Game/FactoryGame/Resource/Parts/GenericBiomass/Desc_Fabric.Desc_Fabric_C",
+"Flower_Petals.png":"/Game/FactoryGame/Resource/Parts/GenericBiomass/Desc_FlowerPetals.Desc_FlowerPetals_C",
+"Gas_Filter.png":"/Game/FactoryGame/Resource/Parts/Filter/Desc_Filter.Desc_Filter_C",
+"Gas_mask.png":"/Game/FactoryGame/Resource/Equipment/GasMask/BP_EquipmentDescriptorGasmask.BP_EquipmentDescriptorGasmask_C",
+"Hazmat_Suit.png":"/Game/FactoryGame/Resource/Equipment/HazmatSuit/BP_EquipmentDescriptorHazmatSuit.BP_EquipmentDescriptorHazmatSuit_C",
+"Heat_Sink.png":"/Game/FactoryGame/Resource/Parts/AluminumPlateReinforced/Desc_AluminumPlateReinforced.Desc_AluminumPlateReinforced_C",
+"Heavy_Modular_Frame.png":"/Game/FactoryGame/Resource/Parts/ModularFrameHeavy/Desc_ModularFrameHeavy.Desc_ModularFrameHeavy_C",
+"High-Speed_Connector.png":"/Game/FactoryGame/Resource/Parts/HighSpeedConnector/Desc_HighSpeedConnector.Desc_HighSpeedConnector_C",
+"Iodine_Infused_Filter.png":"/Game/FactoryGame/Resource/Parts/IodineInfusedFilter/Desc_HazmatFilter.Desc_HazmatFilter_C",
+"Iron_Ingot.png":"/Game/FactoryGame/Resource/Parts/IronIngot/Desc_IronIngot.Desc_IronIngot_C",
+"Iron_Ore.png":"/Game/FactoryGame/Resource/RawResources/OreIron/Desc_OreIron.Desc_OreIron_C",
+"Iron_Plate.png":"/Game/FactoryGame/Resource/Parts/IronPlate/Desc_IronPlate.Desc_IronPlate_C",
+"Iron_Rod.png":"/Game/FactoryGame/Resource/Parts/IronRod/Desc_IronRod.Desc_IronRod_C",
+"Jetpack.png":"/Game/FactoryGame/Resource/Equipment/JetPack/BP_EquipmentDescriptorJetPack.BP_EquipmentDescriptorJetPack_C",
+"Leaves.png":"/Game/FactoryGame/Resource/Parts/GenericBiomass/Desc_Leaves.Desc_Leaves_C",
+"Limestone.png":"/Game/FactoryGame/Resource/RawResources/Stone/Desc_Stone.Desc_Stone_C",
+"Medicinal_Inhaler.png":"/Game/FactoryGame/Equipment/Medkit/Equip_MedKit.Equip_MedKit_C",
+"Modular_Engine.png":"/Game/FactoryGame/Resource/Parts/SpaceElevatorParts/Desc_SpaceElevatorPart_4.Desc_SpaceElevatorPart_4_C",
+"Modular_Frame.png":"/Game/FactoryGame/Resource/Parts/ModularFrame/Desc_ModularFrame.Desc_ModularFrame_C",
+"Motor.png":"/Game/FactoryGame/Resource/Parts/Motor/Desc_Motor.Desc_Motor_C",
+"Mycelia.png":"/Game/FactoryGame/Resource/Parts/GenericBiomass/Desc_Mycelia.Desc_Mycelia_C",
+"Nobelisk.png":"/Game/FactoryGame/Resource/Parts/NobeliskExplosive/Desc_NobeliskExplosive.Desc_NobeliskExplosive_C",
+"Nobelisk_Detonator.png":"/Game/FactoryGame/Resource/Equipment/NobeliskDetonator/BP_EquipmentDescriptorNobeliskDetonator.BP_EquipmentDescriptorNobeliskDetonator_C",
+"Nuclear_Fuel_Rod.png":"/Game/FactoryGame/Resource/Parts/NuclearFuelRod/Desc_NuclearFuelRod.Desc_NuclearFuelRod_C",
+"Nuclear_Waste.png":"/Game/FactoryGame/Resource/Parts/NuclearWaste/Desc_NuclearWaste.Desc_NuclearWaste_C",
+"Object_Scanner.png":"/Game/FactoryGame/Resource/Equipment/GemstoneScanner/BP_EquipmentDescriptorObjectScanner.BP_EquipmentDescriptorObjectScanner_C",
+"Packaged_Fuel.png":"/Game/FactoryGame/Resource/Parts/Fuel/Desc_Fuel.Desc_Fuel_C",
+"Packaged_Heavy_Oil_Residue.png":"/Game/FactoryGame/Resource/Parts/HeavyOilResidue/Desc_PackagedOilResidue.Desc_PackagedOilResidue_C",
+"Packaged_Liquid_Biofuel.png":"/Game/FactoryGame/Resource/Parts/BioFuel/Desc_PackagedBiofuel.Desc_PackagedBiofuel_C",
+"Packaged_Oil.png":"/Game/FactoryGame/Resource/RawResources/CrudeOil/Desc_PackagedOil.Desc_PackagedOil_C",
+"Packaged_Turbofuel.png":"/Game/FactoryGame/Resource/Parts/Turbofuel/Desc_TurboFuel.Desc_TurboFuel_C",
+"Packaged_Water.png":"/Game/FactoryGame/Resource/RawResources/Water/Desc_PackagedWater.Desc_PackagedWater_C",
+"Paleberry.png":"/Game/FactoryGame/Resource/Environment/Berry/Desc_Berry.Desc_Berry_C",
+"Petroleum_Coke.png":"/Game/FactoryGame/Resource/Parts/PetroleumCoke/Desc_PetroleumCoke.Desc_PetroleumCoke_C",
+"Plastic.png":"/Game/FactoryGame/Resource/Parts/Plastic/Desc_Plastic.Desc_Plastic_C",
+"Polymer_Resin.png":"/Game/FactoryGame/Resource/Parts/PolymerResin/Desc_PolymerResin.Desc_PolymerResin_C",
+"Portable_Miner.png":"/Game/FactoryGame/Resource/Equipment/PortableMiner/BP_ItemDescriptorPortableMiner.BP_ItemDescriptorPortableMiner_C",
+"Power_Shard.png":"/Game/FactoryGame/Resource/Environment/Crystal/Desc_CrystalShard.Desc_CrystalShard_C",
+"Quartz_Crystal.png":"/Game/FactoryGame/Resource/Parts/QuartzCrystal/Desc_QuartzCrystal.Desc_QuartzCrystal_C",
+"Quickwire.png":"/Game/FactoryGame/Resource/Parts/HighSpeedWire/Desc_HighSpeedWire.Desc_HighSpeedWire_C",
+"Radio_Control_Unit.png":"/Game/FactoryGame/Resource/Parts/ModularFrameLightweight/Desc_ModularFrameLightweight.Desc_ModularFrameLightweight_C",
+"Raw_Quartz.png":"/Game/FactoryGame/Resource/RawResources/RawQuartz/Desc_RawQuartz.Desc_RawQuartz_C",
+"Rebar_Gun.png":"/Game/FactoryGame/Resource/Equipment/NailGun/Desc_RebarGunProjectile.Desc_RebarGunProjectile_C",
+"Reinforced_Iron_Plate.png":"/Game/FactoryGame/Resource/Parts/IronPlateReinforced/Desc_IronPlateReinforced.Desc_IronPlateReinforced_C",
+"Rifle.png":"/Game/FactoryGame/Resource/Equipment/Rifle/BP_EquipmentDescriptorRifle.BP_EquipmentDescriptorRifle_C",
+"Rifle_Cartridge.png":"/Game/FactoryGame/Resource/Parts/CartridgeStandard/Desc_CartridgeStandard.Desc_CartridgeStandard_C",
+"Rotor.png":"/Game/FactoryGame/Resource/Parts/Rotor/Desc_Rotor.Desc_Rotor_C",
+"Rubber.png":"/Game/FactoryGame/Resource/Parts/Rubber/Desc_Rubber.Desc_Rubber_C",
+"Screw.png":"/Game/FactoryGame/Resource/Parts/IronScrew/Desc_IronScrew.Desc_IronScrew_C",
+"Silica.png":"/Game/FactoryGame/Resource/Parts/Silica/Desc_Silica.Desc_Silica_C",
+"Smart_Plating.png":"/Game/FactoryGame/Resource/Parts/SpaceElevatorParts/Desc_SpaceElevatorPart_1.Desc_SpaceElevatorPart_1_C",
+"Solid_Biofuel.png":"/Game/FactoryGame/Resource/Parts/BioFuel/Desc_Biofuel.Desc_Biofuel_C",
+"Spiked_Rebar.png":"/Game/FactoryGame/Resource/Parts/SpikedRebar/Desc_SpikedRebar.Desc_SpikedRebar_C",
+"Stator.png":"/Game/FactoryGame/Resource/Parts/Stator/Desc_Stator.Desc_Stator_C",
+"Steel_Beam.png":"/Game/FactoryGame/Resource/Parts/SteelPlate/Desc_SteelPlate.Desc_SteelPlate_C",
+"Steel_Ingot.png":"/Game/FactoryGame/Resource/Parts/SteelIngot/Desc_SteelIngot.Desc_SteelIngot_C",
+"Steel_Pipe.png":"/Game/FactoryGame/Resource/Parts/SteelPipe/Desc_SteelPipe.Desc_SteelPipe_C",
+"Sulfur.png":"/Game/FactoryGame/Resource/RawResources/Sulfur/Desc_Sulfur.Desc_Sulfur_C",
+"Supercomputer.png":"/Game/FactoryGame/Resource/Parts/ComputerSuper/Desc_ComputerSuper.Desc_ComputerSuper_C",
+"Turbo_Motor.png":"/Game/FactoryGame/Resource/Parts/MotorLightweight/Desc_MotorLightweight.Desc_MotorLightweight_C",
+"Uranium.png":"/Game/FactoryGame/Resource/RawResources/OreUranium/Desc_OreUranium.Desc_OreUranium_C",
+"Uranium_Cell.png":"/Game/FactoryGame/Resource/Parts/UraniumCell/Desc_UraniumCell.Desc_UraniumCell_C",
+"Uranium_Pellet.png":"/Game/FactoryGame/Resource/Parts/UraniumPellet/Desc_UraniumPellet.Desc_UraniumPellet_C",
+"Versatile_Framework.png":"/Game/FactoryGame/Resource/Parts/SpaceElevatorParts/Desc_SpaceElevatorPart_2.Desc_SpaceElevatorPart_2_C",
+"Wire.png":"/Game/FactoryGame/Resource/Parts/Wire/Desc_Wire.Desc_Wire_C",
+"Wood.png":"/Game/FactoryGame/Resource/Parts/GenericBiomass/Desc_Wood.Desc_Wood_C",
+"Xeno-Basher.png":"/Game/FactoryGame/Resource/Equipment/StunSpear/BP_EquipmentDescriptorStunSpear.BP_EquipmentDescriptorStunSpear_C",
+"Xeno-Zapper.png":"/Game/FactoryGame/Resource/Equipment/ShockShank/BP_EquipmentDescriptorShockShank.BP_EquipmentDescriptorShockShank_C"
+}
+
+# mosaic/mosaic.py writes a pickled list of all the images it used to make the mosaic in row-major order
+image_contents_path = "mosaic/outfile"
+# how many conveyors / pixels high
+height = 50
+
+import pickle
+with open(image_contents_path, 'rb') as f:
+    image_contents = pickle.load(f)
+    # trim off the leading path
+    image_contents = [a.split('/')[-1] for a in image_contents]
+
+# items look like this inside conveyor actor.entity.extra.items
+# items spawn at the input of the conveyor in the order they appear in the save file
+# setting position to different values doesn't seem to do anything
+item_template = """ {{ "resourceName": "{}", "levelName": "", "pathName": "", "position": 1}}"""
+
+# individualy instances of conveyors are named <conveyor_prefix>_<conveyor_number> 
+# you'll have to look at your save file with a visualization to figure these out for your save
+conveyor_prefix = "Persistent_Level:PersistentLevel.Build_ConveyorLiftMk1_C_"
+conveyor_numbers = [114, 113, 112, 111, 110, 109, 108, 107, 106, 105, 104, 103, 102, 101, 100, 99, 98, 97, 96, 95, 94, 93, 92, 91, 90, 89, 88, 87, 86, 85, 84, 83, 82, 81, 80, 79, 78, 77, 76, 75, 74, 73, 72, 71, 70, 69, 68,67,35,2]
+
+import json
+# use https://github.com/ficsit-felix/satisfactory-json sav2json to create a json version of your save file and put it here
+json_save_path = "debug.json"
+with open(json_save_path, 'rb') as f:
+    save_json = json.load(f)
+
+def find_conveyor(conveyor_name):
+    for actor in save_json["actors"]:
+        if actor["pathName"] == conveyor_name:
+            return actor
+def insert_items(items, actor):
+    item_templates = [json.loads(item_template.format(image_to_blueprint_path[item])) for item in items]
+    actor["entity"]["extra"]["items"] = item_templates
+
+row = 0
+items_in_row = len(image_contents)//height
+for conveyor_number in conveyor_numbers:
+    actor = find_conveyor(conveyor_prefix + str(conveyor_number))
+    insert_items(image_contents[row*items_in_row: (row+1)*items_in_row], actor)
+    row += 1
+
+# write a new json save file to this location. use https://github.com/ficsit-felix/satisfactory-json json2sav to convert back into a save file    
+json_save_out_path = "debug_img.json"
+with open(json_save_out_path, 'w') as f:
+    json.dump(obj=save_json, fp=f, indent="  ")
